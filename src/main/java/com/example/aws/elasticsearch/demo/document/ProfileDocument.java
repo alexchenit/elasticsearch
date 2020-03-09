e4c3e76ca7cba74293203241cacef7014c611acf
alexchenit <alexhica@yahoo.ca>
2020-03-08 20:32:07 -0400

    elasticesearch initial

--- src/main/java/com/example/aws/elasticsearch/demo/document/ProfileDocument.java
+++ src/main/java/com/example/aws/elasticsearch/demo/document/ProfileDocument.java
@@ -8,7 +8,62 @@ import com.example.aws.elasticsearch.demo.model.Technologies;
 @Data
 public class ProfileDocument {
 
-    private String id;
+    public ProfileDocument() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "ProfileDocument [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", technologies="
+				+ technologies + ", location=" + location + ", emails=" + emails + "]";
+	}
+	public ProfileDocument(String id, String firstName, String lastName, List<Technologies> technologies,
+			Location location, List<String> emails) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.technologies = technologies;
+		this.location = location;
+		this.emails = emails;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public List<Technologies> getTechnologies() {
+		return technologies;
+	}
+	public void setTechnologies(List<Technologies> technologies) {
+		this.technologies = technologies;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public List<String> getEmails() {
+		return emails;
+	}
+	public void setEmails(List<String> emails) {
+		this.emails = emails;
+	}
+	private String id;
     private String firstName;
     private String lastName;
     private List<Technologies> technologies;
--- src/main/java/com/example/aws/elasticsearch/demo/model/Technologies.java
+++ src/main/java/com/example/aws/elasticsearch/demo/model/Technologies.java
@@ -5,7 +5,32 @@ import lombok.Data;
 @Data
 public class Technologies {
 
-    private String name;
+    public Technologies() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Technologies [name=" + name + ", yearsOfExperience=" + yearsOfExperience + "]";
+	}
+	public Technologies(String name, String yearsOfExperience) {
+		super();
+		this.name = name;
+		this.yearsOfExperience = yearsOfExperience;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+	public void setYearsOfExperience(String yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+	private String name;
     private  String yearsOfExperience;
 
 }
